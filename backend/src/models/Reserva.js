14f20872e92f202965ac8a2533c80cc3bb0993b2
Alvaro <alvarostark13@gmail.com>
2026-03-19 13:10:09 -0300

    Task 04, 05 e 06: Schemas Recurso, Reserva e conexão MongoDB

---
+++ backend/src/models/Reserva.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const ReservaSchema = new mongoose.Schema({
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  recurso: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Recurso",
+    required: true
+  },
+  data: {
+    type: Date,
+    required: true
+  },
+  horaInicio: {
+    type: String,
+    required: true
+  },
+  horaFim: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model("Reserva", ReservaSchema);
